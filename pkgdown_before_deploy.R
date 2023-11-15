7efb1cc62d90ae9968a1a0ff2dfded5a81b78992
marohrdanz <marohrdanz@mdanderson.org>
2023-11-15 09:10:15 -0600

    Try using before_deploy for R-universe
    The build actions are actually failing at the dependencies...so trying
    this suggestion to install from R-Universe.

---
+++ pkgdown_before_deploy.R
@@ -0,0 +1,4 @@
+## Install dependencies from R-Universe
+install.packages('NGCHMSupportFiles', repos = c('https://md-anderson-bioinformatics.r-universe.dev', 'https://cloud.r-project.org'))
+install.packages('NGCHMDemoData', repos = c('https://md-anderson-bioinformatics.r-universe.dev', 'https://cloud.r-project.org'))
+
