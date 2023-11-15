3a2cf884e8307559a5ed8ca4e8812ec20fcf3a54
marohrdanz <marohrdanz@mdanderson.org>
2023-11-15 10:03:52 -0600

    Revert "Try using before_deploy for R-universe"
    This reverts commit 7efb1cc62d90ae9968a1a0ff2dfded5a81b78992.
    
    This commit was an attempt to fix the broken action. But did not.

--- pkgdown_before_deploy.R
+++
@@ -1,4 +0,0 @@
-## Install dependencies from R-Universe
-install.packages('NGCHMSupportFiles', repos = c('https://md-anderson-bioinformatics.r-universe.dev', 'https://cloud.r-project.org'))
-install.packages('NGCHMDemoData', repos = c('https://md-anderson-bioinformatics.r-universe.dev', 'https://cloud.r-project.org'))
-
